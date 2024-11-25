68c6bb4ac44beb3721b455d5766ebe8492819bd8
nsltharaka <nsltharaka@gmail.com>
2024-11-25 18:43:31 +0530

    removed default styles

--- src/components/cart/CartItemCard.tsx
+++ src/components/cart/CartItemCard.tsx
@@ -1,9 +1,11 @@
-import { defaultStyles } from "@/constants/defaultStyles";
 import { useCartContext } from "@/contexts/cartContext/CartContext";
-import { colorConstants, sizeConstants } from "@/theme/styleConstants";
+import {
+  colorConstants,
+  fontConstants,
+  sizeConstants,
+} from "@/theme/styleConstants";
 import type { CartItem } from "@/types/cartItem";
 import { Ionicons } from "@expo/vector-icons";
-import { router } from "expo-router";
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
@@ -16,15 +18,12 @@ export default function CartItemCard({ item }: Props) {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.imageContainer}
-        onPress={() => router.navigate(`/product/${item.id}`)}
-      >
-        <Image source={{ uri: item.mainImage }} style={defaultStyles.image} />
+      <TouchableOpacity style={styles.imageContainer}>
+        <Image source={{ uri: item.mainImage }} style={styles.image} />
       </TouchableOpacity>
 
       <View style={styles.descriptionContainer}>
-        <Text numberOfLines={2} style={defaultStyles.itemName}>
+        <Text numberOfLines={2} style={styles.itemName}>
           {item.name}
         </Text>
         <Text>color : {item.colour}</Text>
@@ -84,16 +83,25 @@ const styles = StyleSheet.create({
     width: 100,
     height: "100%",
   },
+  image: {
+    width: sizeConstants.widthFullScreen,
+    height: "100%",
+    objectFit: "contain",
+  },
+  itemName: {
+    fontWeight: fontConstants.weightBold,
+    fontSize: fontConstants.sizeRegular,
+  },
   descriptionContainer: {
     flex: 1,
     height: "100%",
-    gap: sizeConstants.flexGapSmall,
+    gap: sizeConstants.flexGapMedium,
   },
   quantityAdjuster: {
     flexDirection: "row",
     alignItems: "center",
     gap: sizeConstants.flexGapLarge,
-    marginTop: sizeConstants.marginMedium,
+    marginTop: sizeConstants.marginSmall,
   },
   adjustButton: {
     borderWidth: sizeConstants.widthHairLine,
